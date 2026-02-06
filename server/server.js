0151e2db81c27226048ad1055f8aac3037bc94b3
Sagar Choduhari <sagarchoudhari0@gmail.com>
2026-02-06 14:04:42 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+
+const razorpay = new Razorpay({
+  key_id: "rzp_test_SClIWfk4neJjHH",
+  key_secret: "U6a0IsyfIPp2p4KoN8oJjD4p"
+});
+
+app.post("/create-order", async (req, res) => {
+  const { productId } = req.body;
+
+  const order = await razorpay.orders.create({
+    amount: 900, // ₹9
+    currency: "INR",
+    receipt: productId
+  });
+
+  res.json(order);
+});
+
+app.listen(5000, () => console.log("Server running"));
